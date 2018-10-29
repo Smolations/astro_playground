cca9b8f6f1f51df1ae3587f46cd0b23d33fcd4fe
Chris Smola <callmesmola@gmail.com>
2018-10-29 17:27:54 -0600

    more file and folder renames

--- lib/astro_playground/bodies/body.ex
+++ lib/astro_playground/bodies/body.ex
@@ -1,104 +1,19 @@
-defmodule AstroPlayground.Bodies do
-  @moduledoc """
-  The Example context.
-  """
+defmodule AstroPlayground.Bodies.Language do
+  use Ecto.Schema
+  import Ecto.Changeset
 
-  import Ecto.Query, warn: false
-  alias AstroPlayground.Repo
 
-  alias AstroPlayground.Bodies.Language
+  schema "languages" do
+    field :name, :string
+    field :proverb, :string
 
-  @doc """
-  Returns the list of languages.
-
-  ## Examples
-
-      iex> list_languages()
-      [%Language{}, ...]
-
-  """
-  def list_languages do
-    Repo.all(Language)
+    timestamps()
   end
 
-  @doc """
-  Gets a single language.
-
-  Raises `Ecto.NoResultsError` if the Language does not exist.
-
-  ## Examples
-
-      iex> get_language!(123)
-      %Language{}
-
-      iex> get_language!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_language!(id), do: Repo.get!(Language, id)
-
-  @doc """
-  Creates a language.
-
-  ## Examples
-
-      iex> create_language(%{field: value})
-      {:ok, %Language{}}
-
-      iex> create_language(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_language(attrs \\ %{}) do
-    %Language{}
-    |> Language.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a language.
-
-  ## Examples
-
-      iex> update_language(language, %{field: new_value})
-      {:ok, %Language{}}
-
-      iex> update_language(language, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_language(%Language{} = language, attrs) do
+  @doc false
+  def changeset(language, attrs) do
     language
-    |> Language.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Language.
-
-  ## Examples
-
-      iex> delete_language(language)
-      {:ok, %Language{}}
-
-      iex> delete_language(language)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_language(%Language{} = language) do
-    Repo.delete(language)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking language changes.
-
-  ## Examples
-
-      iex> change_language(language)
-      %Ecto.Changeset{source: %Language{}}
-
-  """
-  def change_language(%Language{} = language) do
-    Language.changeset(language, %{})
+    |> cast(attrs, [:name, :proverb])
+    |> validate_required([:name, :proverb])
   end
 end
